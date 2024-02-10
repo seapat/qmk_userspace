9fffffc66f1b19d522a911386b926b3492e6d627
Sean Klein <63231107+seapat@users.noreply.github.com>
2024-02-10 16:55:45 +0100

    swap layer and ctrl for gaming

--- keyboards/sofle/rev1/keymaps/seapat/keymap.c
+++ keyboards/sofle/rev1/keymaps/seapat/keymap.c
@@ -24,11 +24,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
 
     [_GAME_BASE] = LAYOUT(
-        KC_GRAVE, KC_1,     KC_2,    KC_3,    KC_4,    KC_5,                                                     KC_6,               KC_7,    KC_8,             KC_9,   KC_0,         KC_MINUS,
-        KC_ESC,   KC_T,     KC_Q,    KC_W,    KC_E,    KC_R,                                                     KC_Y,               KC_U,    KC_I,             KC_O,   KC_P,         KC_BSPC,
-        KC_G,     KC_LSFT,  KC_A,    KC_S,    KC_D,    KC_F,                                                     KC_H,               KC_J,    KC_K,             KC_L,   KC_SEMICOLON, KC_QUOTE,
-        KC_TAB,   KC_B,     KC_Z,    KC_X,    KC_C,    KC_V,                    KC_MUTE,  KC_MPLY,               KC_N,               KC_M,    KC_COMM,          KC_DOT, KC_SLSH,      KC_RSFT,
-                            KC_LALT, KC_LGUI, KC_LCTL, LT(_GAME_SWAP, KC_DEL),  KC_SPC,   LT(_QWERTY, KC_ENTER), LT(_SYMB, KC_BSPC), KC_RALT, LT(_EXT, KC_LBRC), KC_RBRC
+        KC_GRAVE, KC_1,     KC_2,    KC_3,    KC_4,                   KC_5,                                               KC_6,               KC_7,    KC_8,             KC_9,   KC_0,         KC_MINUS,
+        KC_ESC,   KC_T,     KC_Q,    KC_W,    KC_E,                   KC_R,                                               KC_Y,               KC_U,    KC_I,             KC_O,   KC_P,         KC_BSPC,
+        KC_G,     KC_LSFT,  KC_A,    KC_S,    KC_D,                   KC_F,                                               KC_H,               KC_J,    KC_K,             KC_L,   KC_SEMICOLON, KC_QUOTE,
+        KC_TAB,   KC_B,     KC_Z,    KC_X,    KC_C,                   KC_V,              KC_MUTE,  KC_MPLY,               KC_N,               KC_M,    KC_COMM,          KC_DOT, KC_SLSH,      KC_RSFT,
+                            KC_LALT, KC_LGUI, LT(_GAME_SWAP, KC_DEL), KC_LCTL, KC_SPC,   LT(_QWERTY, KC_ENTER), LT(_SYMB, KC_BSPC), KC_RALT, LT(_EXT, KC_LBRC), KC_RBRC
     ),
 
     [_GAME_SWAP] = LAYOUT(
